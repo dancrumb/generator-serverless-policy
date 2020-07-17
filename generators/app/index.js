441c7ee0c1c02900368cfe58be45d108ab22a520
Sam Hulick <ffxsam@users.noreply.github.com>
2020-07-17 16:24:05 -0500

    Add s3:DeleteBucketPolicy to policy definition
    This is needed for `sls remove`.

--- generators/app/index.js
+++ generators/app/index.js
@@ -42,6 +42,7 @@ const buildPolicy = ({ name, account, stage, region }) => {
           "s3:ListBucket",
           "s3:GetBucketPolicy",
           "s3:PutBucketPolicy",
+          "s3:DeleteBucketPolicy",
           "s3:ListBucketVersions",
           "s3:PutAccelerateConfiguration",
           "s3:GetEncryptionConfiguration",
